c749df7a7e19e6be86ab80a27fef0657793cef26
jeryslo <jernej.jerin@gmail.com>
2014-05-28 13:22:29 +0200

    Added start project.

---
+++ src/project2/LettersClassification.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package project2;
+
+/**
+ * @author Jernej Jerin
+ * @author Tadej Vodopivec
+ * @author Marija Ðurðeviæ
+ *
+ */
+public class LettersClassification {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Input images
+		
+
+	}
+
+}
